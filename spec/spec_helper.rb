64d569edc8179f84d18e165c1ca3f0cefe819da8
Panayotis Matsinopoulos <panayotis.matsinopoulos@simplybusiness.co.uk>
2016-06-09 11:19:56 +0100

    Get rid of progressbar and use ruby-progressbar instead

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,12 @@
-require 'rubygems'
-require 'bundler'
-Bundler.require(:default, :test)
+require "mongoid"
 
-HOST = ENV['MONGOID_SPEC_HOST'] || 'localhost'
-PORT = ENV['MONGOID_SPEC_PORT'] || '27017'
-DATABASE = 'mongoid_lazy_migration_test'
-
-Mongoid.configure do |config|
-  config.connect_to(DATABASE)
-end
+Mongoid::Config.connect_to("mongoid_lazy_migration_test")
 
 RSpec.configure do |config|
   config.before(:each) do
     Mongoid.purge!
   end
 end
+
+current_path = File.expand_path("..", __FILE__)
+require File.join(current_path, "..", "lib", "mongoid_lazy_migration")
