48a1c01ea95a9b5ec8e72588c075a9f7f81666c7
Peter Vandenberk <peter.vandenberk@simplybusiness.co.uk>
2020-01-07 15:05:51 +0000

    gemstash versioning for dependabot

--- lib/mongoid/lazy_migration/version.rb
+++ lib/mongoid/lazy_migration/version.rb
@@ -1,5 +1,9 @@
 module Mongoid
   module LazyMigration
-    VERSION = "0.8.1"
+    VERSION = "0.8.1" # rubocop:disable Style/MutableConstant
+
+  # SB-specific versioning "algorithm" to accommodate BNW/Jenkins/gemstash
+    VERSION << '.' << ENV['GEM_PRE_RELEASE'].strip \
+    unless ENV.fetch('GEM_PRE_RELEASE', '').strip.empty?
   end
 end
