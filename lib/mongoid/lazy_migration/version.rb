c87eeac176f28ced7c0f20148688e4b567653e3c
Peter Vandenberk <peter.vandenberk@simplybusiness.co.uk>
2020-01-09 13:37:54 +0000

    bump version for new gemstash release

--- lib/mongoid/lazy_migration/version.rb
+++ lib/mongoid/lazy_migration/version.rb
@@ -1,6 +1,6 @@
 module Mongoid
   module LazyMigration
-    base = "0.8.1"
+    base = "0.8.2"
 
   # SB-specific versioning "algorithm" to accommodate BNW/Jenkins/gemstash
     VERSION = (pre = ENV.fetch('GEM_PRE_RELEASE', '')).empty? ? base : "#{base}.#{pre}"
