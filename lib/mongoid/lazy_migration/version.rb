59e5c1d695ad51a5e80112c6b60933cbdad44fb4
Leslie Hoare <leslie.hoare@simplybusiness.co.uk>
2020-03-05 16:35:36 +0000

    Bump version to 0.9.0

--- lib/mongoid/lazy_migration/version.rb
+++ lib/mongoid/lazy_migration/version.rb
@@ -1,6 +1,6 @@
 module Mongoid
   module LazyMigration
-    base = "0.8.2"
+    base = "0.9.0"
 
   # SB-specific versioning "algorithm" to accommodate BNW/Jenkins/gemstash
     VERSION = (pre = ENV.fetch('GEM_PRE_RELEASE', '')).empty? ? base : "#{base}.#{pre}"
